3200e3a0fd971928c7c5c705911713b71891bfdc
Ali Ramazon <mr.safarali@gmail.com>
2023-10-03 10:11:49 -0600

    Setup server and Prisma

---
+++ projects/projectify-app/src/controllers/user.controller.js
@@ -0,0 +1,28 @@
+import { userService } from "../services/user.service.js";
+
+class UserController {
+    signUp = async (req, res) => {
+        const { body } = req;
+
+        const input = {
+            email: body.email,
+            preferreFirstName: body.preferredName,
+            firstName: body.firstName,
+            lastName: body.lastName,
+            password: body.password,
+        };
+
+        try {
+            await userService.signUp(input);
+            res.status(201).json({
+                message: "Success",
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: error.message,
+            });
+        }
+    };
+}
+
+export const userController = new UserController();
