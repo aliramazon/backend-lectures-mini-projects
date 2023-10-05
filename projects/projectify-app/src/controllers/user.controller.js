e106f7895fa55b5220a5a551df0c72ccbf0d0a14
Ali Ramazon <mr.safarali@gmail.com>
2023-10-05 16:25:02 -0600

    Hash the passwords, implement login

--- projects/projectify-app/src/controllers/user.controller.js
+++ projects/projectify-app/src/controllers/user.controller.js
@@ -6,7 +6,7 @@ class UserController {
 
         const input = {
             email: body.email,
-            preferreFirstName: body.preferredName,
+            preferredFirstName: body.preferredName,
             firstName: body.firstName,
             lastName: body.lastName,
             password: body.password,
@@ -19,6 +19,30 @@ class UserController {
             });
         } catch (error) {
             res.status(500).json({
+                message: error.message,
+            });
+        }
+    };
+
+    login = async (req, res) => {
+        const { body } = req;
+        const input = {
+            email: body.email,
+            password: body.password,
+        };
+
+        try {
+            await userService.login(input);
+
+            res.status(200).json({
+                message: "Success",
+            });
+        } catch (error) {
+            let statusCode = 500;
+            if (error.message === "Invalid Credentials") {
+                statusCode = 401;
+            }
+            res.status(statusCode).json({
                 message: error.message,
             });
         }
