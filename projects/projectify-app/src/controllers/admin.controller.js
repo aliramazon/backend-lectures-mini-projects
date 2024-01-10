5e592b06f68cf30ab4759bdd5a83e824886a403f
Ali Ramazon <mr.safarali@gmail.com>
2024-01-09 20:22:21 -0500

    Fix bug: Admin  reset-password, activate account, signup

--- projects/projectify-app/src/controllers/admin.controller.js
+++ projects/projectify-app/src/controllers/admin.controller.js
@@ -17,12 +17,13 @@ class AdminController {
 
         if (body.company) {
             companyInput.name = body.company.name;
-            companyInput.position = body.company.position;
+            companyInput.position = body.company.name;
         }
 
         await adminService.signUp(adminInput, companyInput);
         res.status(201).json({
-            message: "Success",
+            message:
+                "We have just sent you an email. Please,  Activate your account.",
         });
     });
 
@@ -63,7 +64,8 @@ class AdminController {
         await adminService.forgotPassword(email);
 
         res.status(200).json({
-            message: "Password reset email has been sent",
+            message:
+                "We emailed you an instruction to reset your password. Follow it!",
         });
     });
 
--- projects/projectify-app/src/utils/mailer.js
+++ projects/projectify-app/src/utils/mailer.js
@@ -11,6 +11,11 @@ class Mailer {
                 pass: process.env.MAILER_PASS,
             },
         });
+        this.baseApiURL =
+            process.env.NODE_ENV === "local"
+                ? "http://localhost:4000"
+                : "https://projectify-app-api.onrender.com";
+        this.baseUiURL = process.env.UI_BASE_URL;
     }
     send = async (mailOptions) => {
         try {
@@ -25,7 +30,7 @@ class Mailer {
             await this.send({
                 to: emailAddress,
                 subject: "Projectify App | Activate Your Account",
-                html: `<a style="color: red;" href="http://localhost:4000/admins/activate?activationToken=${token}">Verify your email</a>`,
+                html: `<a style="color: red;" href="${this.baseApiURL}/admins/activate?activationToken=${token}">Verify your email</a>`,
             });
         } catch (error) {
             throw error;
@@ -37,7 +42,7 @@ class Mailer {
             this.send({
                 to: emailAddress,
                 subject: "Projectify App | Reset Password",
-                html: `<a href="http://localhost:3000/reset-password/passwordResetToken=${token}">Reset Your Password</a>`,
+                html: `<a href="${this.baseUiURL}/admin/reset-password?passwordResetToken=${token}">Reset Your Password</a>`,
             });
         } catch (error) {
             throw error;
@@ -49,7 +54,7 @@ class Mailer {
             await this.send({
                 to: emailAddress,
                 subject: "Projectify App | Welcome to the team",
-                html: `<a href="http://localhost:3000/team-member/create-password?inviteToken=${token}">Click to create a password</a>`,
+                html: `<a href="${this.baseUiURL}/team-member/create-password?inviteToken=${token}">Click to create a password</a>`,
             });
         } catch (error) {
             throw error;
